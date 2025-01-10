876ee112c8a3bee43cc242f65fce4c961947b813
Edson Ottoni <43525138+EdsonOttoni@users.noreply.github.com>
2025-01-10 14:51:47 -0300

    finalizando sign-in/up page

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,6 @@
 import { Loader2 } from "lucide-react"
-import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs"
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs"
+import Image from "next/image"
 
 export default function SignInPage() {
   return (
@@ -14,7 +15,7 @@ export default function SignInPage() {
         
         <div className="flex items-center justify-center mt-8">
           <ClerkLoaded>
-            <SignUp path="/sign-in"/>
+            <SignIn path="/sign-in"/>
           </ClerkLoaded>
           <ClerkLoading>
             <Loader2 className="animate-spin text-muted-foreground"/>
@@ -22,8 +23,9 @@ export default function SignInPage() {
         </div>
       </div>
 
-      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
-        
+      <div className="h-full flex-col space-y-4 bg-blue-600 hidden lg:flex items-center justify-center">
+        <Image alt="logo" src='/logo.svg' height={100} width={100}/>
+        <h1 className="text-3xl text-white tracking-wider">Finance</h1>
       </div>
     </div>
   )
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,32 @@
-import { SignUp } from "@clerk/nextjs"
+import { Loader2 } from "lucide-react"
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs"
+import Image from "next/image"
 
 export default function SignUpPage() {
   return (
-    <SignUp path="/sign-up"/>
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-slate-600">Welcome back</h1>
+          <p className="text-base text-slate-400">
+            Log in or Create account to get back to your dashboard
+          </p>
+        </div>
+        
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignUp path="/sign-up"/>
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground"/>
+          </ClerkLoading>
+        </div>
+      </div>
+
+    <div className="h-full flex-col space-y-4 bg-blue-600 hidden lg:flex items-center justify-center">
+            <Image alt="logo" src='/logo.svg' height={100} width={100}/>
+            <h1 className="text-3xl text-white tracking-wider">Finance</h1>
+          </div>
+    </div>
   )
 }
