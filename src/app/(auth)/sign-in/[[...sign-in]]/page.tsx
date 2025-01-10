df45843a20fd6b6ec5884e052030fbce3ed8d62c
EdsonOttoni <edson.ottoni@gmail.com>
2025-01-10 01:43:44 -0300

    add loader in sign in

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,5 @@
-import { SignUp } from "@clerk/nextjs"
+import { Loader2 } from "lucide-react"
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs"
 
 export default function SignInPage() {
   return (
@@ -6,13 +7,23 @@ export default function SignInPage() {
       <div className="h-full lg:flex flex-col items-center justify-center px-4">
         <div className="text-center space-y-4 pt-16">
           <h1 className="font-bold text-3xl text-slate-600">Welcome back</h1>
+          <p className="text-base text-slate-400">
+            Log in or Create account to get back to your dashboard
+          </p>
         </div>
-        <p className="text-base text-slate-400">
-          Log in or Create account to get back to your dashboard
-        </p>
-        <div className="flex items-center justify-normal mt-8">
-          <SignUp path="/sign-in"/>
+        
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignUp path="/sign-in"/>
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground"/>
+          </ClerkLoading>
         </div>
+      </div>
+
+      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+        
       </div>
     </div>
   )
